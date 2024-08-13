0c5ff79bf6b6e64abea3ac505630a216283596b8
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-13 10:56:33 +0100

    feat: Turn off audio clips.

---
+++ PhantomSilence/Harmony/Patch.cs
@@ -0,0 +1,28 @@
+using System;
+using HarmonyLib;
+
+namespace PhantomSilence.Harmony
+{
+    [HarmonyPatch(typeof(AudioObject), nameof(AudioObject.Update))]
+    public static class Patch
+    {
+        private static readonly ILogger Logger = new Logger();
+
+        private static readonly string[] TargetNames =
+        {
+            "Night_Oneshots", // a_daybreak_oneshot[1-6]
+            "Dusk_Stingers", // a_nightfall_oneshot
+            "Dawn_Stingers" // a_daybreak_oneshot
+        };
+
+        public static bool Prefix(AudioObject __instance, float deltaTime)
+        {
+            if (!Array.Exists(TargetNames, name => name == __instance.name)) return true;
+
+            Logger.Info($"Muting {__instance.name} using TurnOff and SetVolume.");
+            __instance.TurnOff(true);
+
+            return true;
+        }
+    }
+}
